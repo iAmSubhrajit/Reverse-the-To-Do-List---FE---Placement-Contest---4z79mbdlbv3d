58e84482dacf873e6ef945b308e236bf4b91a688
Subhrajit Das <113223195+iAmSubhrajit@users.noreply.github.com>
2023-03-12 15:42:49 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/App.css';
 
-function ToDo() {
+function ToDo({ id, createdAt }) {
+  const [inputValue, setinputValue] = useState('')
   return (<tr>
     <td>
-      <p>id</p>
+      <p>{id}</p>
     </td>
     <td>
-      <input />
+      <input onChange={(e) => setinputValue(e.target.value)} value={inputValue} />
     </td>
     <td>
-      <p>createdAt</p>
+      <p>{createdAt}</p>
     </td>
   </tr>)
 }
@@ -27,9 +28,12 @@ function App() {
 
   return (
     <div id="main">
-      <button>Reverse</button>
+      <button onClick={() => setTodos([...todos].reverse())}>Reverse</button>
       <table>
         <tbody>
+          {todos.map((el) => (
+            <ToDo key={el.id} id={el.id} createdAt={el.createdAt} />
+          ))}
         </tbody>
       </table>
     </div>
